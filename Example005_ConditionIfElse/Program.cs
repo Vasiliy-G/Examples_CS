cbc44a598a85d1b36ff3ce3d8818c668c2b35911
kotedengi <kotedengi@email.com>
2022-08-21 18:58:57 +0500

    add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+	//Console.Writeline("Ура, это же МАША!");
+	Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+	Console.Write("Привет, ");
+	Console.WriteLine(username);
+}
