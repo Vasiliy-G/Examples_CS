c3ffb5e5c40658b82b44f8fbc4ed09ece18b9425
kotedengi <kotedengi@email.com>
2022-08-24 20:36:50 +0500

    add Seminar_2

---
+++ Seminar_2/Test1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Канкатинация и Интерполяция
+
+int number1 = 10;
+int number2 = 20;
+
+Console.WriteLine(number1 + " " + number2);  // Канкатинация
+
+Console.WriteLine($"Число1: {number1}, Число2: {number2}");  //  Интерполяция
