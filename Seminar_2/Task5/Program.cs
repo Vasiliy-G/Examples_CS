6e5c3b31d2a1ae4de0110a7d1c0a69deaf8a8d14
kotedengi <kotedengi@email.com>
2022-08-24 20:26:00 +0500

    add Seminar_2

---
+++ Seminar_2/Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет,
+// является ли одно число квадратом другого.
+
+int number1 = new Random().Next(10, 100);
+//int number1 = 81;
+Console.WriteLine(number1);
+
+int number2 = new Random().Next(10, 100);
+//int number2 = 9;
+Console.WriteLine(number2);
+
+if(number1 * number1 == number2 || number2 * number2 == number1)
+{
+    Console.WriteLine("Является");
+}
+
+else
+{
+     Console.WriteLine("Не является");
+}
