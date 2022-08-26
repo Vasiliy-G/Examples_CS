c878c6b026fdee83e200e2f1093f1d45e0fdf180
kotedengi <kotedengi@email.com>
2022-08-26 21:54:01 +0500

    add Task23

---
+++ homework/Task23/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+void MethodOne(int number)
+{
+    int count = 1;
+    while(count <= number)
+    {
+        double result = Math.Pow(count, 3);
+        Console.WriteLine(result);
+        count++;
+    }
+}
+
+
+void MethodTwo(int number)
+{
+    for (int count = 1; count <= number; count++)
+    {
+        double result = Math.Pow(count, 3);
+        Console.WriteLine(result);
+    }
+}
+
+
+Console.Write("Введите число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Решение способом №1:");
+MethodOne(number);
+
+Console.WriteLine("Решение способом №2:");
+MethodTwo(number);
