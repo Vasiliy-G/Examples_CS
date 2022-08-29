1a75fa29ce81acca2174c8c83cc87cbe0afa4a57
kotedengi <kotedengi@email.com>
2022-08-30 04:46:52 +0500

    add Task29

---
+++ homework/Task29/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+void ArrayRandom(int[] array)
+{
+    int arrayLength = array.Length;
+    
+    for(int index = 0; index < arrayLength; index++)
+    {
+        array[index] = new Random().Next(0, 101); 
+    }
+}
+
+
+void ArrayPrint(int[] array)
+{
+    int arrayLength = array.Length;
+    
+    for(int index = 0; index < arrayLength; index++)
+    {
+        Console.Write(array[index]);
+        if(index != arrayLength - 1)
+        {
+            Console.Write(", ");
+        }
+    }
+}
+
+
+int [] array = new int[8];
+
+ArrayRandom(array);
+ArrayPrint(array);
