194010667dea2e9ea4410b7ba482a95d593566d8
kotedengi <kotedengi@email.com>
2022-09-05 18:09:54 +0500

    add Example013_RecursionAlgorithm

---
+++ Example013_RecursionAlgorithm/Example/Program.cs
@@ -0,0 +1,53 @@
+﻿// Двумерные массивы
+
+
+void PrintArrayTable(string[,] table)
+{
+    for (int rows = 0; rows < 2; rows++)
+    {
+        for (int columns = 0; columns < 5; columns++)
+        {
+            Console.WriteLine($"-{table[rows, columns]}-");
+        }
+    }
+}
+
+
+// String.Empty
+string[,] table = new string[2, 5];  // 2 строки, 5 столбцы
+table[1, 2] = "слово";
+PrintArrayTable(table);
+
+
+void PrintArrayMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)  // matrix.GetLength(0) длинна строк
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)  // matrix.GetLength(1) длинна столбцов
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+
+void FillArrayMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)  // matrix.GetLength(0) длинна строк
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)  // matrix.GetLength(1) длинна столбцов
+        {
+           matrix[i ,j] = new Random().Next(1, 10);  // [1; 10)]
+        }
+    }
+}
+
+
+int[,] matrix = new int[3, 4];  // 3 строки, 4 столбцы
+
+PrintArrayMatrix(matrix);
+FillArrayMatrix(matrix);
+Console.WriteLine();
+PrintArrayMatrix(matrix);
---
+++ Example013_RecursionAlgorithm/Task2_Factorial/Program.cs
@@ -0,0 +1,18 @@
+﻿// Факториал 5! = 5 * 4 * 3 * 2 * 1
+
+
+double Factorial(int n)
+{
+    // 1! = 1
+    // 0! = !
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+
+Console.WriteLine(Factorial(3));  // 1 * 2 * 3 = 6
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
